b6d5661e628b097e4ee86dbf69a5f652af5caff7
Sierra <sierra.ar.sager@gmail.com>
2024-12-19 20:42:22 -0500

    completed part 2

--- js/assignment.js
+++ js/assignment.js
@@ -9,10 +9,12 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
+const book = "The Martian";
 
 
 // Declare a mutable variable named "activity" and assign
 // an activity to it.
+let activity = "Crafting";
 
 
 
@@ -25,12 +27,14 @@ function updateBookSentence() {
 function updateActivitySentence() {
     // Update this function 
     // (hint: it will be similiar to the previous function)
+    activityAnswerEl.innerText = activity
 
 }
 
 function render() {
     // Finish writing this function
     updateBookSentence();
+    updateActivitySentence();
 
 }
 
